3280eb9ebe838812a933a5bff75bfbe2021ad722
nishnata-nandi <nishnata.nandi90@gmail.com>
2025-04-23 22:30:15 +0200

    style updates and page division

--- script.js
+++
@@ -1,8 +0,0 @@
- <script>
-    AOS.init();
-    function toggleCard(card) {
-      const isActive = card.classList.contains('active');
-      document.querySelectorAll('.card').forEach(c => c.classList.remove('active'));
-      if (!isActive) card.classList.add('active');
-    }
-  </script>
